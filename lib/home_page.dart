5758ea4d3325fb2f1842c4837ee7cd959ee14295
sss <bigdatachobo@gmail.com>
2023-03-30 11:03:50 +0900

    step3

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,47 +4,75 @@ import 'qr_scanner_page.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final fontSize = screenWidth * 0.8 / 3;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => QRScannerPage(
-                          isInbound: true,
+                Expanded(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => QRScannerPage(
+                            isInbound: true,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: Colors.lightBlue,
+                      child: Text(
+                        '입고',
+                        style: TextStyle(
+                          fontSize: fontSize,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    );
-                  },
-                  child: const Text('입고'),
+                    ),
+                  ),
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => QRScannerPage(
-                          isInbound: false,
+                Expanded(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => QRScannerPage(
+                            isInbound: false,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: Colors.lightGreen,
+                      child: Text(
+                        '출고',
+                        style: TextStyle(
+                          fontSize: fontSize,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                    );
-                  },
-                  child: const Text('출고'),
+                    ),
+                  ),
                 ),
               ],
             ),
-            ElevatedButton(
-              onPressed: () {
+          ),
+          Expanded(
+            child: InkWell(
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
@@ -54,10 +82,20 @@ class HomePage extends StatelessWidget {
                   ),
                 );
               },
-              child: const Text('이동'),
+              child: Container(
+                alignment: Alignment.center,
+                color: Colors.redAccent[100],
+                child: Text(
+                  '이동',
+                  style: TextStyle(
+                    fontSize: screenWidth * 1.5 / 3,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
